a2f993a011f05b70a3be5bca79855b555d7e1f9f
Efraim Olcer <54858420+efraimolcer@users.noreply.github.com>
2024-02-06 23:01:32 +0300

    Lecture Samples: 'Hours & Minutes' & 'Volume Price' are practiced. [BLG 102 | 02. Inputs & Outputs]

---
+++ blg102_intro_to_science_engineering_computation/lecture/02_2_volume_price.c
@@ -0,0 +1,24 @@
+/*	QUESTION
+	Write a C program that calculates the price for each ounce of a pack.
+		1. Calculate the price for each ounce of a pack.
+		2. Print out the price for each ounce.
+	+ One pack has 6 cans. (Constant)
+	+ The price of a pack is 1998 dollar.
+	+ Every can has 333 ounce volume.
+*/
+
+#include	<stdio.h>
+#include	<stdlib.h>	// EXIT_SUCCESS
+
+int main(){
+	const double	CANS_PER_PACK = 6;								// Given integer as default
+	double			pack_price = 1998.0,							// Given float as default
+					can_volume = 333.0;								// Given float as default
+	
+	double			pack_volume = can_volume * CANS_PER_PACK;
+	double			price_per_ounce = pack_price / pack_volume;
+	
+	printf("Price per ounce: %.2f\n", price_per_ounce);
+	
+	return EXIT_SUCCESS;
+}
