2a5a50ec275bc4e5e11697c4a9752b491a9bfb34
Efraim Olcer <54858420+efraimolcer@users.noreply.github.com>
2024-02-07 22:56:01 +0300

    Recitation Practices: 'Fahrenheit to Celsius', 'Roots of Second Degree Polynomial' & 'Body Surface Area' are solved. [BLG 102 | 02. Inputs & Outpus]

--- blg102_intro_to_science_engineering_computation/lecture/02_1_hours_minutes.c
+++ blg102_intro_to_science_engineering_computation/lecture/02_1_hours_minutes.c
@@ -9,14 +9,16 @@
 #include	<stdio.h>
 
 int main(){
-	const int MINUTES_IN_HOUR = 60;				// Always constant
-	int duration = 0;
+	const int	MINUTES_IN_HOUR = 60;			// Always constant
+	int			hours,
+				minutes,
+				duration = 0;
 	
 	printf("Enter duration [min]: ");
 	scanf("%d", &duration);
 	
-	int hours = duration / MINUTES_IN_HOUR;		// Get integer division which means hours
-	int minutes = duration % MINUTES_IN_HOUR;	// Get remainder which means minutes
+	hours = duration / MINUTES_IN_HOUR;			// Get integer division which means hours
+	minutes = duration % MINUTES_IN_HOUR;		// Get remainder which means minutes
 	
 	printf("%d minutes equal to %d hours %d minutes\n", duration, hours, minutes);
 	
